c43a6718f4ec6fb9bde5bb94b68d992f4d627091
Viviana Londoño <sandrislc@gmail.com>
2017-11-10 17:41:47 -0500

    test home status code and test get all rooms status code

---
+++ ColombiaResortApiTest.py
@@ -0,0 +1,38 @@
+import app
+import os
+import unittest
+#import sys
+#Anexo el Directorio en donde se encuentra la clase a llamar
+#sys.path.append('.app')
+
+
+class ColombiaResortTestCase(unittest.TestCase):
+
+  def setUp(self):
+    # creates a test client
+    self.cliente = app.app.test_client()
+    # propagate the exceptions to the test client
+    app.app.config['TESTING'] = True
+
+  def tearDown(self):
+    pass
+
+
+  def test_home_status_code(self):
+    # sends HTTP GET request to the application
+    # on the specified path
+    result = self.cliente.get('/')
+
+    # assert the status code of the response
+    self.assertEqual(result.status_code, 404)
+
+  def test_get_all_rooms_status_code(self):
+    # sends HTTP GET request to the application
+    # on the specified path
+    result = self.cliente.get('/V1/AllRooms')
+
+    # assert the status code of the response
+    self.assertEqual(result.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
