16ca610a3fe8a5a3487b762826663041b2392705
Viviana Londoño <sandrislc@gmail.com>
2017-11-13 23:02:05 -0500

    test result get room null

--- ColombiaResortApiTest.py
+++ ColombiaResortApiTest.py
@@ -1,9 +1,6 @@
 import app
 import unittest
-#import sys
-#Anexo el Directorio en donde se encuentra la clase a llamar
-#sys.path.append('.app')
-
+import json
 
 class ColombiaResortTestCase(unittest.TestCase):
 
@@ -34,19 +31,24 @@ class ColombiaResortTestCase(unittest.TestCase):
 
     # assert the status code of the response
     self.assertEqual(result.status_code, 200)
-    print(result.data)
+    #print(result.data)
 
 
-  # #Testing status code in path get rooms acording to parameters
+  #Testing status code in path get rooms acording to parameters
   def test_get_rooms_with_parameters_status_code(self):
     # sends HTTP GET request to the application
     # on the specified path
     result = self.cliente.get('/V1/rooms/?room_type=S&hosts=4&city=05001&arrive_date=2017-12-12&leave_date=2017-12-12')
-
-     # assert the status code of the response
+    # assert the status code of the response
     self.assertEqual(result.status_code, 200)
-    print(result.data)
 
+  #Testing not rooms available
+  def test_get_rooms_is_null(self):
+    result = self.cliente.get('/V1/rooms/?room_type=S&hosts=4&city=05001&arrive_date=2017-12-12&leave_date=2017-12-12')
+    # assert the status code of the response
+    data = json.loads(result.get_data(as_text=True))
+    self.assertEqual(data['rooms'], [])
+    print(data['rooms'])
 
 
 if __name__ == '__main__':
